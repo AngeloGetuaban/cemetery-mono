84098d721aae3e6b3e04604f62f050f4ec1ccaa9
south-ventures <114236024+AngeloGetuaban@users.noreply.github.com>
2025-09-19 15:42:44 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -79,5 +79,5 @@ app.use(errorHandler);
 // ---- start
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+  console.log(`🚀 Server listening on http://localhost:${PORT}/visitor/home`);
 });
